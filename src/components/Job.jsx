bc6484e639ad7dd34fc060c6ac9e95950d16df28
front-ed <edwinschutjes@gmail.com>
2023-07-17 14:45:51 +0200

    style: general info for job component

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -10,7 +10,26 @@ const Job = ({
   languages,
   tools,
 }) => {
-  return <article>{company}</article>;
+  return (
+    <article className="job-wrapper">
+      <div className="general-info-wrapper">
+        <img src={logo} alt={company} className="img" />
+        <div className="col">
+          <h2>{company}</h2>
+          <h3>{position}</h3>
+          <div className="time-location-wrapper">
+            <p>{postedAt}</p>
+            <div className="dot">{/* CSS dot */}</div>
+            <p>{contract}</p>
+            <div className="dot">{/* CSS dot */}</div>
+            <p>{location}</p>
+          </div>
+        </div>
+      </div>
+      {/* SB */}
+      <div className="labels-wrapper">labels</div>
+    </article>
+  );
 };
 
 export default Job;
